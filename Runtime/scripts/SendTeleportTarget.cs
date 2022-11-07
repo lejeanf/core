582147d62835727c94455ce54c73701cc8183d3d
Jean-Francois Robin <jean-francois_robin@uqar.ca>
2022-11-07 13:15:25 +0200

    upgraded player: now can teleport, move around, snap turn...

--- Runtime/scripts/SendTeleportTarget.cs
+++ Runtime/scripts/SendTeleportTarget.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 namespace jeanf.core{
     public class SendTeleportTarget : MonoBehaviour
     {
         [SerializeField] string currentScene;
+
         public delegate void TeleportPlayer(Transform teleportTarget);
         public static event TeleportPlayer teleportPlayer;
 
         private void Awake()
         {
             teleportPlayer?.Invoke(this.transform);
+        }
+        private void OnDrawGizmos()
+        {
+           Gizmos.color = new Color(.2f, .6f, .2f, .5f);
+           Gizmos.DrawSphere(transform.position, .25f);
         }
     }
 
