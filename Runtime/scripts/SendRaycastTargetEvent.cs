f573ab2393db1b1ff9db143104d5ddf1421a02f4
Jean-Francois Robin <jean-francois_robin@uqar.ca>
2022-11-24 13:35:58 +0200

    raycast send event

--- Runtime/scripts/SendRaycastTargetEvent.cs
+++ Runtime/scripts/SendRaycastTargetEvent.cs
@@ -2,28 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider))]
-public class SendRaycastTarget : MonoBehaviour
-{
-    private Collider collider;
-    public delegate void SendRaycastTargetEvent(Collider colliders);
-    public static SendRaycastTargetEvent sendRaycastTarget;
-    public static SendRaycastTargetEvent removeRaycastTarget;
-    private void Awake()
+namespace jeanf.core 
+{ 
+    [RequireComponent(typeof(Collider))]
+    public class SendRaycastTargetEvent : MonoBehaviour
     {
-        collider = this.gameObject.GetComponent<Collider>();
-    }
+        private Collider collider;
+        public delegate void SendRaycastTarget(Collider colliders);
+        public static SendRaycastTarget sendRaycastTarget;
+        public static SendRaycastTarget removeRaycastTarget;
+        private void Awake()
+        {
+            collider = this.gameObject.GetComponent<Collider>();
+        }
 
-    private void OnEnable()
-    {
-        sendRaycastTarget?.Invoke(collider);
-    }
-    private void OnDestroy()
-    {
-        removeRaycastTarget?.Invoke(collider);
-    }
-    private void OnDisable()
-    {
-        removeRaycastTarget?.Invoke(collider);
+        private void OnEnable()
+        {
+            sendRaycastTarget?.Invoke(collider);
+        }
+        private void OnDestroy()
+        {
+            removeRaycastTarget?.Invoke(collider);
+        }
+        private void OnDisable()
+        {
+            removeRaycastTarget?.Invoke(collider);
+        }
     }
 }
--- Runtime/scripts/TriggerEventOnRaycast.cs
+++ Runtime/scripts/TriggerEventOnRaycast.cs
@@ -3,82 +3,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-
-public class TriggerEventOnRaycast : MonoBehaviour
+namespace jeanf.core
 {
-
-    public GameObject cameraForward;
-    List<GameObject> targetList = new();
-    private bool isCurrentTargetInSight = false;
-    private bool lastTargetStatus = false;
-
-    public UnityEvent TargetInSight;
-    public UnityEvent TargetInOutOfSight;
-
-
-    private void OnEnable()
+    public class TriggerEventOnRaycast : MonoBehaviour
     {
-        SendRaycastTarget.sendRaycastTarget += UpdateCurrentTarget;
-        SendRaycastTarget.removeRaycastTarget += RemoveCurrentTarget;
-    }
-    private void OnDisable() => Unsubscribe();
-    private void OnDestroy() => Unsubscribe();
-    void Unsubscribe()
-    {
-        SendRaycastTarget.sendRaycastTarget -= UpdateCurrentTarget;
-        SendRaycastTarget.removeRaycastTarget -= RemoveCurrentTarget;
-    }
 
-    void UpdateCurrentTarget(Collider collider)
-    {
-        if (!targetList.Contains(collider.gameObject))
+        public GameObject cameraForward;
+        List<GameObject> targetList = new();
+        private bool isCurrentTargetInSight = false;
+        private bool lastTargetStatus = false;
+
+        public UnityEvent TargetInSight;
+        public UnityEvent TargetInOutOfSight;
+
+
+        private void OnEnable()
         {
-            targetList.Add(collider.gameObject);
+            SendRaycastTargetEvent.sendRaycastTarget += UpdateCurrentTarget;
+            SendRaycastTargetEvent.removeRaycastTarget += RemoveCurrentTarget;
         }
-    }
-    void RemoveCurrentTarget(Collider collider)
-    {
-        if (targetList.Contains(collider.gameObject))
+        private void OnDisable() => Unsubscribe();
+        private void OnDestroy() => Unsubscribe();
+        void Unsubscribe()
         {
-            targetList.Remove(collider.gameObject);
+            SendRaycastTargetEvent.sendRaycastTarget -= UpdateCurrentTarget;
+            SendRaycastTargetEvent.removeRaycastTarget -= RemoveCurrentTarget;
         }
-    }
 
-    private void LateUpdate()
-    {
-        FireRay();
-    }
-
-    void FireRay()
-    {
-        if (targetList.Count <= 0 || targetList == null) return;
-
-        Ray ray = new Ray(cameraForward.transform.position, cameraForward.transform.forward);
-        RaycastHit hit;
-        Physics.Raycast(ray, out hit);
-
-        if ( Physics.Raycast(cameraForward.transform.position, cameraForward.transform.forward, 100)) {
-
-            Debug.DrawRay(cameraForward.transform.position, cameraForward.transform.forward * hit.distance);
-            if (hit.transform.gameObject && targetList.Contains(hit.transform.gameObject))
+        void UpdateCurrentTarget(Collider collider)
+        {
+            if (!targetList.Contains(collider.gameObject))
             {
-                isCurrentTargetInSight = true;
+                targetList.Add(collider.gameObject);
             }
-            else
+        }
+        void RemoveCurrentTarget(Collider collider)
+        {
+            if (targetList.Contains(collider.gameObject))
             {
-                isCurrentTargetInSight = false;
+                targetList.Remove(collider.gameObject);
             }
-            if (isCurrentTargetInSight)
+        }
+
+        private void LateUpdate()
+        {
+            FireRay();
+        }
+
+        void FireRay()
+        {
+            if (targetList.Count <= 0 || targetList == null) return;
+
+            Ray ray = new Ray(cameraForward.transform.position, cameraForward.transform.forward);
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit);
+
+            if (Physics.Raycast(cameraForward.transform.position, cameraForward.transform.forward, 100))
             {
-                TargetInSight?.Invoke();
-                Debug.Log($"{hit.transform.gameObject.name} is in sight.");
-                lastTargetStatus = true;
-            }
-            else
-            {
-                TargetInOutOfSight?.Invoke();
-                Debug.Log($"{hit.transform.gameObject.name} is not in sight.");
-                lastTargetStatus = false;
+
+                Debug.DrawRay(cameraForward.transform.position, cameraForward.transform.forward * hit.distance);
+                if (hit.transform.gameObject && targetList.Contains(hit.transform.gameObject))
+                {
+                    isCurrentTargetInSight = true;
+                }
+                else
+                {
+                    isCurrentTargetInSight = false;
+                }
+                if (isCurrentTargetInSight)
+                {
+                    TargetInSight?.Invoke();
+                    lastTargetStatus = true;
+                }
+                else
+                {
+                    TargetInOutOfSight?.Invoke();
+                    lastTargetStatus = false;
+                }
             }
         }
     }
